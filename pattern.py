fafe7673f59232d737f57bc0a2e71a87173650b4
James Moore <james@panic.com>
2016-06-24 12:43:19 -0600

    added totals

--- pattern.py
+++ pattern.py
@@ -1,52 +1,53 @@
+import sys
 from random import random
 
 import os
-from jinja2 import Environment, Template, FileSystemLoader
-
 import yaml
+from jinja2 import Environment, FileSystemLoader
 
 input_file = open('config.yaml')
 
 config = yaml.load(input_file)
 
-print config
-
-rows = config['height'] / config['tilesize']
-cols = config['width'] / config['tilesize']
+# print config
+tilesize = config['tilesize']
+height = config['height']
+width = config['width']
 sections = len(config['color_weights'][0])
 num_colors = len(config['color_weights'])
-
+rows = height / tilesize
+cols = width / tilesize
 section_length = rows / sections
-print "Rows: %s   Cols: %s  Colors: %s   Sections: %s   Section Length: %s" % (
-rows, cols, num_colors, sections, section_length)
 
-# make grid
+if height % tilesize != 0:
+    sys.exit("height not divisible by tilesize")
+elif width % tilesize != 0:
+    sys.exit("width not divisible by tilesize")
+elif height % sections != 0:
+    sys.exit("height not divisible by number of sections")
+
+print "Rows: %s   Cols: %s  Colors: %s   Sections: %s   Section Length: %s" % (
+    rows, cols, num_colors, sections, section_length)
+
 grid = [[0 for x in range(cols)] for y in range(rows)]
-# for row_idx, row in enumerate(grid):
-#     print 'grid', row
 
 zipped_colors = zip(*config['color_weights'])
-print "zipped colors", zipped_colors
 
 section_totals = map(sum, zipped_colors)
-# print section_totals
 
-# make buckets
 buckets = [[0 for x in range(num_colors)] for y in range(sections)]
+tile_counts = [0 for x in range(num_colors + 1)]
 
 for s_idx, total in enumerate(section_totals):
 
     whole = 0.0
 
     for c_idx, color_weight in enumerate(zipped_colors[s_idx]):
-
         ratio = color_weight / float(total)
         value = whole + ratio
         whole += ratio
 
         buckets[s_idx][c_idx] = value
-
-print 'buckets', buckets
 
 section_num = 0
 
@@ -56,14 +57,18 @@ for s_idx, section in enumerate(buckets):
         row_n = (s_idx * section_length) + r_idx
 
         for c_idx in range(cols):
-            # print "Section: %s   Row: %s   Col: %s" % (s_idx, r_idx, c_idx)
             r = random()
 
             for color_idx, p in enumerate(section):
                 if r <= p:
                     grid[row_n][c_idx] = color_idx
+                    tile_counts[color_idx] += 1
                     break
 
+tile_count_total = sum(tile_counts)
+
+total_sq_ft = width * height / 144
+total_cost = total_sq_ft * float(config['price_per_sq_ft'])
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 env = Environment(loader=FileSystemLoader(THIS_DIR), trim_blocks=True)
@@ -72,14 +77,14 @@ context = {
     'colors': config['colors'],
     'grid': grid,
     'section_length': section_length,
+    'tilesize': tilesize,
+    'tile_counts': tile_counts,
+    'tile_count_total': tile_count_total,
+    'total_cost': total_cost,
+
 }
 
 rendered_template = env.get_template('output.html.j2').render(context)
-# print rendered_template
 
 with open('output.html', 'w') as f:
     f.write(rendered_template)
-
-# for row in range(rows):
-#     print grid[row]
-
